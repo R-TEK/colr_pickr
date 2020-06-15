9823162451fd28f113308e00b7330c3c9f45dbbc
R-TEK <doughtycallum.1999@gmail.com>
2020-06-15 22:21:08 +0100

    Add function to change color from hex input

--- src/js/color_text_values.js
+++ src/js/color_text_values.js
@@ -64,36 +64,52 @@ document.getElementById('switch_color_type').addEventListener('click', function 
 	switchColorType();
 });
 
-// TODO: Make Comments for the below
+// Event to update the color when the user leaves the hex value box
+document.getElementById('hex_input').addEventListener('blur', function () {
+	// Value
+	const hexInput = this.value;
 
-document.getElementById('hex_input').addEventListener('blur', function (event) {
-	// TODO: Find a HEXA Regex
+	// Check to see if the hex is formatted correctly
+	if (hexInput.match(/^#[0-9a-f]{3}([0-9a-f]{3})?([0-9a-f]{2})?$/)) {
+		// Updating the picker
+		updateColorDisplays(hexInput);
+	}
 });
 
+// Gathering all the rgba inputs boxes
 document.querySelectorAll('.rgba_input').forEach((element) => {
+	// Event to update the color when the user changes the value to any of the input boxes
 	element.addEventListener('change', function () {
+		// Input boxes
 		const rgbaInput = document.querySelectorAll('.rgba_input');
 
+		// Checking that the numbers are within the correct boundaries
 		if (rgbaInput[0].value > 255) throw 'Value must be below 256';
 		if (rgbaInput[1].value > 255) throw 'Value must be below 256';
 		if (rgbaInput[2].value > 255) throw 'Value must be below 256';
 		if (rgbaInput[3].value > 1) throw 'Value must be equal to or below 1';
 
+		// Updating the picker
 		updateColorDisplays(
 			`rgba(${rgbaInput[0].value}, ${rgbaInput[1].value}, ${rgbaInput[2].value}, ${rgbaInput[3].value})`
 		);
 	});
 });
 
+// Gathering all the hsla inputs boxes
 document.querySelectorAll('.hsla_input').forEach((element) => {
+	// Event to update the color when the user changes the value to any of the input boxes
 	element.addEventListener('change', function () {
+		// Input boxes
 		const hslaInput = document.querySelectorAll('.hsla_input');
 
+		// Checking that the numbers are within the correct boundaries
 		if (hslaInput[0].value > 359) throw 'Value must be below 360';
 		if (hslaInput[1].value > 100) throw 'Value must be below 100';
 		if (hslaInput[2].value > 100) throw 'Value must be below 100';
 		if (hslaInput[3].value > 1) throw 'Value must be equal to or below 1';
 
+		// Updating the picker
 		updateColorDisplays(
 			`rgba(${hslaInput[0].value}, ${hslaInput[1].value}%, ${hslaInput[2].value}%, ${hslaInput[3].value})`
 		);
