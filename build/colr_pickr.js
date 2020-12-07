0c516fc7541012806f8314dc4a94f11fbb122861
R-TEK <doughtycallum.1999@gmail.com>
2020-12-07 14:26:38 +0000

    Added logic behind having the picker show up underneath the picker

--- build/colr_pickr.js
+++ build/colr_pickr.js
@@ -41,17 +41,31 @@ function ColorPicker(element, color) {
 	element.style.background = color;
 
 	// Click listener to have the button open the color picker interface
-	element.addEventListener('click', function (event) {
-		console.log('picker opened');
+	element.addEventListener('click', function () {
 		// Applying the items instance to the color picker object
-		colorPickerComp.instance = event.target.colorPickerObj;
+		colorPickerComp.instance = this.colorPickerObj;
+
+		// Define picker
+		const picker = document.getElementById('color_picker');
 
 		// Displaying the color picker
-		document.getElementById('color_picker').style.display = 'block';
-		document.getElementById('color_picker_bg').style.display = 'block';
+		picker.style.display = 'block';
+
+		// Find position of button
+		let top = this.getBoundingClientRect().top;
+		let left = this.getBoundingClientRect().left;
+
+		// Taking in height so the picker is underneath
+		top = top + this.offsetHeight + 2;
+
+		console.log(top)
+		console.log(left)
+
+		picker.style.top = top + 'px';
+		picker.style.left = left + 'px';
 
 		// Updating the color picker
-		colorPickerComp.updateColorDisplays(event.target.getAttribute('data-color'));
+		colorPickerComp.updateColorDisplays(this.getAttribute('data-color'));
 	});
 }
 
@@ -90,7 +104,7 @@ function ColorPicker(element, color) {
 					<rect x="0" y="0" width="100%" height="100%" fill="url(#brightness)"></rect>
 				</pattern>
 			</defs>
-			<rect rx="5" ry="5" x="1" y="1" width="348" height="185" stroke="#fff" stroke-width="2" fill="url(#pattern_config)"></rect>
+			<rect rx="5" ry="5" x="1" y="1" width="263" height="130" stroke="#fff" stroke-width="2" fill="url(#pattern_config)"></rect>
 			<svg id="box_dragger" x="336" y="14" style="overflow: visible;">
 				<circle r="9" fill="none" stroke="#000" stroke-width="2"></circle>
 				<circle r="7" fill="none" stroke="#fff" stroke-width="2"></circle>
@@ -188,7 +202,7 @@ function ColorPicker(element, color) {
 		</div>
 		<div id="custom_colors">
 			<div id="custom_colors_header">
-				<svg id="custom_colors_pallet_icon" viewBox="0 0 24 24" width="18" height="18">
+				<svg id="custom_colors_pallet_icon" viewBox="0 0 24 24" width="15" height="18">
 					<path fill="#555" d="M4 21.832c4.587.38 2.944-4.493 7.188-4.538l1.838 1.534c.458 5.538-6.315 6.773-9.026 3.004zm14.065-7.115c1.427-2.239 5.847-9.749 5.847-9.749.352-.623-.43-1.273-.976-.813 0 0-6.572 5.714-8.511 7.525-1.532 1.432-1.539 2.086-2.035 4.447l1.68 1.4c2.227-.915 2.868-1.039 3.995-2.81zm-11.999 3.876c.666-1.134 1.748-2.977 4.447-3.262.434-2.087.607-3.3 2.547-5.112 1.373-1.282 4.938-4.409 7.021-6.229-1-2.208-4.141-4.023-8.178-3.99-6.624.055-11.956 5.465-11.903 12.092.023 2.911 1.081 5.571 2.82 7.635 1.618.429 2.376.348 3.246-1.134zm6.952-15.835c1.102-.006 2.005.881 2.016 1.983.004 1.103-.882 2.009-1.986 2.016-1.105.009-2.008-.88-2.014-1.984-.013-1.106.876-2.006 1.984-2.015zm-5.997 2.001c1.102-.01 2.008.877 2.012 1.983.012 1.106-.88 2.005-1.98 2.016-1.106.007-2.009-.881-2.016-1.988-.009-1.103.877-2.004 1.984-2.011zm-2.003 5.998c1.106-.007 2.01.882 2.016 1.985.01 1.104-.88 2.008-1.986 2.015-1.105.008-2.005-.88-2.011-1.985-.011-1.105.879-2.004 1.981-2.015zm10.031 8.532c.021 2.239-.882 3.718-1.682 4.587l-.046.044c5.255-.591 9.062-4.304 6.266-7.889-1.373 2.047-2.534 2.442-4.538 3.258z"/>
 				</svg>
 				<button id="custom_colors_add" name="add-a-custom-color">
@@ -201,7 +215,7 @@ function ColorPicker(element, color) {
 			</div>
 		</div>
 		<div id="color_context_menu" class="color_ctx_menu">
-			<button id="color_clear_single" class="color_ctx_menu" name="remove-single-color">Remove Color</button>
+			<button id="color_clear_single" class="color_ctx_menu" name="remove-single-color">Remove</button>
 			<button id="color_clear_all" class="color_ctx_menu" name="remove-all-colors">Remove All</button>
 		</div>
 	`;
@@ -211,11 +225,6 @@ function ColorPicker(element, color) {
 	colorPickerContainer.id = 'color_picker';
 	colorPickerContainer.innerHTML = HTMLContent;
 	document.getElementsByTagName('BODY')[0].appendChild(colorPickerContainer);
-
-	// Creating a darken background node
-	const colorPickerBackground = document.createElement('DIV');
-	colorPickerBackground.id = 'color_picker_bg';
-	document.getElementsByTagName('BODY')[0].appendChild(colorPickerBackground);
 
 	// Checking if a local storage variable has been set
 	if (localStorage.getItem('custom_colors') === null) {
@@ -250,6 +259,8 @@ document.addEventListener('mousedown', function () {
 	}
 });
 
+//TODO: needs to be converted to a click anywhere to shut the color picker
+/*
 // Click the darken background to close the color picker
 document.getElementById('color_picker_bg').addEventListener('click', function () {
 	// Hiding elements
@@ -267,7 +278,7 @@ document.getElementById('color_picker_bg').addEventListener('click', function ()
 		a: colorPickerComp.alpha
 	});
 });
-
+*/
 /**
  * Custom Color Change Event
  */
