ce28eafd5cfdd7ecd6d92383104abc484fd8503c
R-TEK <doughtycallum.1999@gmail.com>
2020-12-16 22:50:11 +0000

    Reduced setup files size

--- build/colr_pickr.js
+++ build/colr_pickr.js
@@ -22,6 +22,8 @@
  */
 let colorPickerComp = new Object();
 
+// TODO: add jsdoc for options
+
 /**
  * @description Colr Pickr Constructor
  * @param {HTMLElement} element - Define the button that you want to open this instance of the color picker
@@ -158,7 +160,7 @@ function ColorPicker(element, color, options) {
 				</pattern>
 			</defs>
 			<rect rx="5" ry="5" x="0" y="2" width="263" height="130" stroke="#fff" stroke-width="2" fill="url(#pattern_config)"></rect>
-			<svg id="box_dragger" x="336" y="14" style="overflow: visible;">
+			<svg id="box_dragger" x="336" y="14">
 				<circle r="9" fill="none" stroke="#000" stroke-width="2"></circle>
 				<circle r="7" fill="none" stroke="#fff" stroke-width="2"></circle>
 			</svg>
@@ -178,7 +180,7 @@ function ColorPicker(element, color, options) {
 					</linearGradient>
 				</defs>
 				<rect rx="5" ry="5" x="2" y="1" width="262" height="20" stroke="#fff" stroke-width="2" fill="url(#hue)"></rect>
-				<svg id="color_slider_dragger" x="277" y="11" style="overflow: visible;">
+				<svg id="color_slider_dragger" x="277" y="11">
 					<circle r="7" fill="none" stroke="#000" stroke-width="2"></circle>
 					<circle r="5" fill="none" stroke="#fff" stroke-width="2"></circle>
 				</svg>
@@ -191,7 +193,7 @@ function ColorPicker(element, color, options) {
 					</linearGradient>
 				</defs>
 				<rect rx="5" ry="5" x="2" y="6" width="262" height="10" stroke="#fff" stroke-width="2" fill="url(#opacity)"></rect>
-				<svg id="opacity_slider_dragger" x="277" y="11" style="overflow: visible;">
+				<svg id="opacity_slider_dragger" x="277" y="11">
 					<circle r="7" fill="none" stroke="#000" stroke-width="2"></circle>
 					<circle r="5" fill="none" stroke="#fff" stroke-width="2"></circle>
 				</svg>
@@ -201,50 +203,50 @@ function ColorPicker(element, color, options) {
 			<div id="hexa">
 				<input id="hex_input" name="hex_input" type="text" maxlength="9" spellcheck="false" />
 				<br>
-				<label for="hex_input" class="label_text">HEX</label>
+				<label for="hex_input">HEX</label>
 			</div>
-			<div id="rgba" style="display: none;">
-				<div class="rgba_divider">
+			<div id="rgba">
+				<div>
 					<input class="rgba_input" name="r" type="number" min="0" max="255" />
 					<br>
-					<label for="r" class="label_text">R</label>
+					<label for="r">R</label>
 				</div>
-				<div class="rgba_divider">
+				<div>
 					<input class="rgba_input" name="g" type="number" min="0" max="255" />
 					<br>
-					<label for="g" class="label_text">G</label>
+					<label for="g">G</label>
 				</div>
-				<div class="rgba_divider">
+				<div>
 					<input class="rgba_input" name="b" type="number" min="0" max="255" />
 					<br>
-					<label for="b" class="label_text">B</label>
+					<label for="b">B</label>
 				</div>
-				<div class="rgba_divider">
+				<div>
 					<input class="rgba_input" name="a" type="number" step="0.1" min="0" max="1" />
 					<br>
-					<label for="a" class="label_text">A</label>
+					<label for="a">A</label>
 				</div>
 			</div>
-			<div id="hsla" style="display: none;">
-				<div class="hsla_divider">
+			<div id="hsla">
+				<div>
 					<input class="hsla_input" name="h" type="number" min="0" max="359" />
 					<br>
-					<label for="h" class="label_text">H</label>
+					<label for="h">H</label>
 				</div>
-				<div class="hsla_divider">
+				<div>
 					<input class="hsla_input" name="s" type="number" min="0" max="100" />
 					<br>
-					<label for="s" class="label_text">S%</label>
+					<label for="s">S%</label>
 				</div>
-				<div class="hsla_divider">
+				<div>
 					<input class="hsla_input" name="l" type="number" min="0" max="100" />
 					<br>
-					<label for="l" class="label_text">L%</label>
+					<label for="l">L%</label>
 				</div>
-				<div class="rgba_divider">
+				<div>
 					<input class="hsla_input" name="a" type="number" step="0.1" min="0" max="1" />
 					<br>
-					<label for="a" class="label_text">A</label>
+					<label for="a">A</label>
 				</div>
 			</div>
 			<button id="switch_color_type" class="remove_outline" name="switch-color-type">
@@ -255,7 +257,7 @@ function ColorPicker(element, color, options) {
 		</div>
 		<div id="custom_colors">
 			<div id="custom_colors_header">
-				<svg id="custom_colors_pallet_icon" viewBox="0 0 24 24" width="15" height="18">
+				<svg class="pallet_icon" viewBox="0 0 24 24" width="15" height="18">
 					<path fill="#555" d="M4 21.832c4.587.38 2.944-4.493 7.188-4.538l1.838 1.534c.458 5.538-6.315 6.773-9.026 3.004zm14.065-7.115c1.427-2.239 5.847-9.749 5.847-9.749.352-.623-.43-1.273-.976-.813 0 0-6.572 5.714-8.511 7.525-1.532 1.432-1.539 2.086-2.035 4.447l1.68 1.4c2.227-.915 2.868-1.039 3.995-2.81zm-11.999 3.876c.666-1.134 1.748-2.977 4.447-3.262.434-2.087.607-3.3 2.547-5.112 1.373-1.282 4.938-4.409 7.021-6.229-1-2.208-4.141-4.023-8.178-3.99-6.624.055-11.956 5.465-11.903 12.092.023 2.911 1.081 5.571 2.82 7.635 1.618.429 2.376.348 3.246-1.134zm6.952-15.835c1.102-.006 2.005.881 2.016 1.983.004 1.103-.882 2.009-1.986 2.016-1.105.009-2.008-.88-2.014-1.984-.013-1.106.876-2.006 1.984-2.015zm-5.997 2.001c1.102-.01 2.008.877 2.012 1.983.012 1.106-.88 2.005-1.98 2.016-1.106.007-2.009-.881-2.016-1.988-.009-1.103.877-2.004 1.984-2.011zm-2.003 5.998c1.106-.007 2.01.882 2.016 1.985.01 1.104-.88 2.008-1.986 2.015-1.105.008-2.005-.88-2.011-1.985-.011-1.105.879-2.004 1.981-2.015zm10.031 8.532c.021 2.239-.882 3.718-1.682 4.587l-.046.044c5.255-.591 9.062-4.304 6.266-7.889-1.373 2.047-2.534 2.442-4.538 3.258z"/>
 				</svg>
 				<button id="custom_colors_add" class="remove_outline" name="add-a-custom-color">
